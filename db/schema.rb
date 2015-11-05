fb00f320b216791fcc20d63a0a4da485475ff423
AdrianMLin <adrian.mh.lin@gmail.com>
2015-11-04 23:54:28 -0500

    DATABASE: categories now have color column ||| inputs have been adjusted to input it too

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151026170202) do
+ActiveRecord::Schema.define(version: 20151105044302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20151026170202) do
     t.string  "name"
     t.integer "user_id"
     t.string  "icon"
+    t.string  "color"
   end
 
   create_table "entries", force: true do |t|
