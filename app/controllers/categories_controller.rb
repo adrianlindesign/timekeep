b944571ddf7bdac86c7de637f3b0166317f0bf5c
AdrianMLin <adrian.mh.lin@gmail.com>
2015-11-06 01:31:21 -0500

    SETTINGS > CATEGORIES: users cannot edit category names to be the same as already existing categories fix 2

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,13 +10,13 @@ class CategoriesController < ApplicationController
 
   def create
     user_categories = Category.where( {user_id: params['user_id']} ) 
-    same_category = user_categories.find_by( {name: params['name']} )
+    same_category_name = user_categories.find_by( {name: params['name']} )
 
-    if same_category  # if category with same user_id and name exists, update icon
-      same_category.update( {
+    if same_category_name  # if category with same user_id and name exists, update icon
+      same_category_name.update( {
           icon: params['icon'],  
           color: params['color']
-      })  #TODO: make it UPDATEABLE!!!!!!!!!!!!!!
+      })  
       puts 'Category icon updated'
     else #if category with same user_id and name doesn't exist, create new
       c = Category.create(
@@ -46,10 +46,12 @@ class CategoriesController < ApplicationController
   end
 
   def update
+    binding.pry
+    user_categories = Category.where( {user_id: params['user_id']} ) 
+    same_category_name = user_categories.find_by( {name: params['name']} )
 
-    alreadyExists = Category.find_by(name: params['name']);
-
-    unless alreadyExists
+   
+    unless same_category_name
       c = Category.find( params[:id] )
       c.update({
         name: params['name'],
